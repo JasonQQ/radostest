710c8db65ff6fa4834bebde8f75eec637fc9a024
wuxw3 <wuxw3@lenovo.com>
2019-08-27 15:45:40 +0800

    read api

--- src/main/java/com/example/radostest/CephApiContoller.java
+++ src/main/java/com/example/radostest/CephApiContoller.java
@@ -51,7 +51,7 @@ public class CephApiContoller {
             System.out.println("io Ctx created");
 
             io.write(fileName, bytes);
-            System.out.println("io write");
+            System.out.println("io write " + bytes.length);
 
             cluster.ioCtxDestroy(io);
             System.out.println("io Ctx destroyed");
@@ -60,6 +60,38 @@ public class CephApiContoller {
             System.out.println(e.getMessage() + ": " + e.getReturnValue());
         }
         return result;
+
+    }
+
+    @PostMapping("/pool/{pool}/fileName/{fileName}/read")
+    public byte[] read(
+            @PathVariable String pool,
+            @PathVariable String fileName,
+            @RequestBody int length) {
+        byte[] buf = new byte[length];
+        try {
+            Rados cluster = new Rados("admin");
+            System.out.println("Create cluster handle.");
+            File conf = new File("/etc/ceph/ceph.conf");
+            cluster.confReadFile(conf);
+            System.out.println("Read the Ceph config file.");
+
+            cluster.connect();
+            System.out.println("Connected to the Ceph cluster");
+
+            IoCTX io = cluster.ioCtxCreate(pool);
+            System.out.println("io Ctx created");
+
+            int len = io.read(fileName, length, 0, buf);
+            System.out.println("io read " + length);
+
+            cluster.ioCtxDestroy(io);
+            System.out.println("io Ctx destroyed");
+
+        } catch (RadosException e) {
+            System.out.println(e.getMessage() + ": " + e.getReturnValue());
+        }
+        return buf;
 
     }
 }
