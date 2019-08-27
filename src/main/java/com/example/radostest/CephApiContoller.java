c09f87d45fab98e18b629a528acee1611fdfe963
wuxw3 <wuxw3@lenovo.com>
2019-08-27 11:04:35 +0800

    write api

--- src/main/java/com/example/radostest/CephApiContoller.java
+++ src/main/java/com/example/radostest/CephApiContoller.java
@@ -1,9 +1,9 @@
 package com.example.radostest;
 
+import com.ceph.rados.IoCTX;
 import com.ceph.rados.Rados;
 import com.ceph.rados.exceptions.RadosException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 
@@ -28,5 +28,38 @@ public class CephApiContoller {
             System.out.println(e.getMessage() + ": " + e.getReturnValue());
         }
         return result;
+    }
+
+    @PostMapping("/fileName/{fileName}/write")
+    public String write(
+            @PathVariable String pool,
+            @PathVariable String fileName,
+            @RequestBody byte[] bytes) {
+        String result = "Failed";
+        try {
+            Rados cluster = new Rados("admin");
+            System.out.println("Create cluster handle.");
+            File conf = new File("/etc/ceph/ceph.conf");
+            cluster.confReadFile(conf);
+            System.out.println("Read the Ceph config file.");
+
+            cluster.connect();
+            result = "Connected to the Ceph cluster";
+            System.out.println(result);
+
+            IoCTX io = cluster.ioCtxCreate(pool);
+            System.out.println("io Ctx created");
+
+            io.write(fileName, bytes);
+            System.out.println("io write");
+
+            cluster.ioCtxDestroy(io);
+            System.out.println("io Ctx destroyed");
+
+        } catch (RadosException e) {
+            System.out.println(e.getMessage() + ": " + e.getReturnValue());
+        }
+        return result;
+
     }
 }
